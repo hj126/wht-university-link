6137a6477f639080f732d1d7bdc5b3a6d874a932
hj126 <hj126@live.com>
2019-04-21 22:16:49 +0800

    Update 18008.js

--- univ/18008.js
+++ univ/18008.js
@@ -2,19 +2,25 @@ var univ_list = {
     school: "中原工学院",
     id: "18008",
     link: [ {
-        name: "石大主页",
-        url: "http://www.shzu.edu.cn/"
+        name: "中工主页",
+        url : "https://www.zut.edu.cn/"
     }, {
         name: "教务处",
-        url: "http://jwc.shzu.edu.cn/"
+        url : "http://202.196.32.50/"
+    }, {
+        name: "网上服务大厅",
+        url : "http://i.zut.edu.cn"
+    }, {
+        name: "中工邮箱",
+        url : "http://mail.zut.edu.cn/"
+    }, {
+        name: "网络教学平台",
+        url : "http://study.zut.edu.cn/portal"
     }, {
         name: "图书馆",
-        url: "http://lib.shzu.edu.cn/"
+        url : "https://lib.zut.edu.cn/"
     }, {
-        name: "在线教育平台",
-        url: "http://eol.shzu.edu.cn/meol//index.do"
-    }, {
-        name: "英语教学平台",
-        url: "http://unipus.shzu.edu.cn/"
+        name: "中工FTP",
+        url : "http://dk.zut.edu.cn/"
     } ]
 };
